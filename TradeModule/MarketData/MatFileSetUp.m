c64c297efee50d85633be1c07ef48277aa141bce
natwijaya19 <72418586+natwijaya19@users.noreply.github.com>
2022-02-25 17:23:04 +0700

    Update

---
+++ TradeModule/MarketData/MatFileSetUp.m
@@ -0,0 +1,11 @@
+classdef MatFileSetUp
+    %UNTITLED4 Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        path  = pwd;
+        fileName = "DataInput\PriceVolumeInput.mat";    
+    end
+    
+end
+
---
+++ TradeModule/MarketData/SpreadSheetSetUp.m
@@ -0,0 +1,17 @@
+classdef SpreadSheetSetUp
+    %UNTITLED2 Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        fileName = "DataInput\PriceVolumeInput.xlsx"
+        path = pwd;
+        openPriceSheet = "openPrice"
+        lowPriceSheet = "lowPrice"
+        highPriceSheet = "highPrice"
+        closePriceSheet = "closePrice"
+        volumeSheet = "volume"
+        IndexIHSGSheet = "IndexIHSG"
+    end
+    
+end
+
